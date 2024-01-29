526a48147277984c36f6077b123634d2fbbdf752
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-01-29 23:28:32 +0100

    Add registered user to file

--- PersonalBudget.cpp
+++ PersonalBudget.cpp
@@ -5,7 +5,7 @@ void PersonalBudget::registerUser()
     User user = enterNewUserData();
 
     users.push_back(user);
-    //writeNewUserInFile(user);
+    writeNewUserInFile(user);
 
     cout << endl << "Account created" << endl << endl;
     system("pause");
@@ -59,6 +59,29 @@ string PersonalBudget::readLine()
     string input = "";
     getline(cin, input);
     return input;
+}
+
+void PersonalBudget::writeNewUserInFile(User user)
+{
+    CMarkup xml;
+    bool fileExist = xml.Load(usersFilename);
+
+    if (!fileExist)
+    {
+        xml.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+        xml.AddElem("users");
+    }
+    xml.FindElem();
+    xml.IntoElem();
+    xml.AddElem("user");
+    xml.IntoElem();
+    xml.AddElem("userId", user.getUserId());
+    xml.AddElem("login", user.getLogin());
+    xml.AddElem("password", user.getPassword());
+    xml.AddElem("name", user.getName());
+    xml.AddElem("surname", user.getSurname());
+
+    xml.Save(usersFilename);
 }
 
 void PersonalBudget::showUsers()
--- PersonalBudget.h
+++ PersonalBudget.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 
+#include "Markup.h"
 #include "User.h"
 
 using namespace std;
@@ -12,13 +13,20 @@ class PersonalBudget
 {
     int loggedInUserId;
     vector <User> users;
+    string usersFilename;
 
     User enterNewUserData();
     bool checkUsername(string loginSuggestion);
     int readNewUserId();
     string readLine();
+    void writeNewUserInFile(User user);
 
 public:
+    PersonalBudget()
+    {
+        usersFilename = "users.xml";
+    }
+
     void registerUser();
     void showUsers();
 };
